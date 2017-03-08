d666688d788a4631f79733e48a0d8ecf08e028ce
cimcim <cimcim@192.168.0.23>
2017-03-07 21:58:37 -0500

    error log tester not functional due to system.exit

---
+++ Enamel/src/enamel/simulator/ErrorLogTest.java
@@ -0,0 +1,43 @@
+package enamel.simulator;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class ErrorLogTest {
+	
+	SoundPlayer PlayerFive = new SoundPlayer();
+	SoundPlayer PlayerSix = new SoundPlayer();
+	SoundPlayer PlayerSeven = new SoundPlayer();
+	
+
+	@Test
+	public void TestErrorLogFive() throws IOException, FileNotFoundException{
+		PlayerFive.startFile("Scenario_Five.txt");
+		@SuppressWarnings("resource")
+		BufferedReader Check = new BufferedReader(new FileReader(System.getProperty("user.dir") + "\\" + "ERROR_LOG.txt"));
+		assertTrue(Check.readLine() != null);
+	}
+	
+	@Test
+	public void TestErrorLogSix() throws IOException, FileNotFoundException{
+		PlayerSix.startFile("Scenario_Six.txt");
+		@SuppressWarnings("resource")
+		BufferedReader Check = new BufferedReader(new FileReader(System.getProperty("user.dir") + "\\" + "ERROR_LOG.txt"));
+		assertTrue(Check.readLine() != null);
+	}
+	
+	@Test
+	public void TestErrorLogSeven() throws IOException, FileNotFoundException{
+		PlayerSeven.startFile("Scenario_Seven.txt");
+		@SuppressWarnings("resource")
+		BufferedReader Check = new BufferedReader(new FileReader(System.getProperty("user.dir") + "\\" + "ERROR_LOG.txt"));
+		assertTrue(Check.readLine() != null);
+	}
+	
+}
